af7e44db7df1b450f00ac907f48076fb0d1a0b08
vansonnguyen22 <vansonnguyen220905@gmail.com>
2026-03-04 16:04:20 +0700

    config ingress

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 // CORS (nên đưa origin sang env sau)
 app.use(
   cors({
-    origin: "/",
+    origin: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
