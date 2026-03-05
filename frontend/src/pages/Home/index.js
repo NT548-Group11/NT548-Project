9cec7a29e1257c56659d467b3b559addf0030762
vansonnguyen22 <vansonnguyen220905@gmail.com>
2026-03-05 09:49:03 +0700

    feat(project): remove icon

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -64,7 +64,7 @@ function Home() {
                   Hãy bắt đầu hành trình thay đổi bản thân ngay hôm nay!
                 </p>
                 <Button className="cta-button" type="primary" size="large">
-                  <NavLink to="/login">Khám Phá Ngay🐧</NavLink>
+                  <NavLink to="/login">Khám Phá Ngay 🐧</NavLink>
                 </Button>
               </Col>
               <Col xs={24} md={12} className="home-image">
