79f2a8bf987b094e4b17a7beb85e56d6af83f62a
Ba_Quoc <lebaquoc1009@gmail.com>
2026-04-23 13:17:21 +0700

    tesst8

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "./reducers";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  window.matchMedia =
+    window.matchMedia ||
+    function (query) {
+      return {
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: function () {}, // deprecated
+        removeListener: function () {},
+        addEventListener: function () {},
+        removeEventListener: function () {},
+        dispatchEvent: function () {},
+      };
+    };
+});
+
+const store = createStore(allReducers);
+
+test("renders app", () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+  );
 });
