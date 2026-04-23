84c5d8dc84ae2cd3f82883d3ec1df070a8dee3c9
Ba_Quoc <lebaquoc1009@gmail.com>
2026-04-23 13:21:37 +0700

    tesst9

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,35 +1,11 @@
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import allReducers from "./reducers";
+import App from "./App";
 
-beforeAll(() => {
-  window.matchMedia =
-    window.matchMedia ||
-    function (query) {
-      return {
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: function () {}, // deprecated
-        removeListener: function () {},
-        addEventListener: function () {},
-        removeEventListener: function () {},
-        dispatchEvent: function () {},
-      };
-    };
-});
-
-const store = createStore(allReducers);
-
-test("renders app", () => {
+test("render app", () => {
   render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   );
 });
